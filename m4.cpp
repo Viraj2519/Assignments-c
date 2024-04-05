5bb7b19267767b42da687d370a77f639758c4362
Viraj Solanki <virajsolanki2519@gmail.com>
2024-04-05 13:59:38 -0400

    The functions do not work and i'm confused on what to do to correct them, there are multiple errors

--- m4.cpp
+++ m4.cpp
@@ -7,11 +7,12 @@
 struct FlightData {
 	char source[50];
 	char destination[50];
-	int fare;
+	int  fare;
 };
 
 int parseLine(char* source, char* destination, int* fare, char* line);
-
+int processFlight(char filename, struct Flight flights[], int numFlights);
+void displayLeastFareDetails();
 int main(void) {
 	struct FlightData data {};
 	FILE* flightFile;
@@ -22,7 +23,7 @@ int main(void) {
 		return 0;
 	}
 	char SingleLine[40];
-	char Textdetails[40];
+	char line[40];
 	while (fgets(SingleLine, 40, flightFile) != 0) {
 		if (SingleLine[strlen(SingleLine) - 1] == '\n') {
 
@@ -33,18 +34,18 @@ int main(void) {
 			printf("Error opening file: %s", SingleLine);
 			return 0; // Move to the next line in flights.txt
 		}
-		while (fgets(Textdetails, sizeof(Textdetails), fp) != 0) {
-			parseLine(data.source, data.destination, &data.fare, Textdetails);
-		}
+		fgets(line, 40, fp);
+		parseLine(data.source, data.destination, &data.fare, line);
 	}
-	
-		return 0;
-	}
-		int parseLine(char* source, char* destination, int* fare, char* line) {
-			if (source == NULL || destination == NULL || fare == NULL || line == NULL) {
-				return -1;
-			}
+	return 0;
+}
 
-			return 0; // Success
-		}
-	
+
+void processFlights(char filename,struct FlightData details[], int numFlights) {
+	parseLine(details->source, details->destination, &details->fare, &filename);
+
+}
+void displayLeastFareDetails()
+{
+}
+
